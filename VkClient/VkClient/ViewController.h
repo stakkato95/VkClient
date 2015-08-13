b98c7893dd270f0ca6e0d24d57fc5f74748c242a
Artsiom Kaliaha <stakkato95@gmail.com>
2015-08-13 16:37:58 +0300

    project setup

---
+++ VkClient/VkClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VkClient
+//
+//  Created by Artsiom Kaliaha on 13/08/15.
+//  Copyright (c) 2015 Artsiom Kaliaha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
