7af05e2ee10e8966908f0592286676abcbfd45fa
Artsiom Kaliaha <stakkato95@gmail.com>
2015-08-15 22:00:14 +0300

    added blocks, some classes renamed to suit name conventions

---
+++ VkClient/VkClient/Common/VKCIdentifiable.h
@@ -0,0 +1,13 @@
+//
+//  Identifying.h
+//  VkClient
+//
+//  Created by Artsiom Kaliaha on 15/08/15.
+//  Copyright (c) 2015 Artsiom Kaliaha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol Identifying <NSObject>
+
+@end
---
+++ VkClient/VkClient/VKCBlocks.h
@@ -0,0 +1,13 @@
+//
+//  Blocks.h
+//  VkClient
+//
+//  Created by Artsiom Kaliaha on 15/08/15.
+//  Copyright (c) 2015 Artsiom Kaliaha. All rights reserved.
+//
+
+#ifndef VkClient_Blocks_h
+#define VkClient_Blocks_h
+
+
+#endif
